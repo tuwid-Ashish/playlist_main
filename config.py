23f8317243ab71556054cc447f85d3cb2514e291
tuwid <ashishchumber9@gmail.com>
2025-01-28 00:55:14 +0530

    small env fix

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "spotify client secret")
     SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI", "spotify_redirect_uri")
     HF_API_TOKEN = os.environ.get("HF_ACCESS_TOKEN")
-    SPOTIFY_USER_ID = "xq7dp9tfkzicm5s75ng50ix0k"
+    SPOTIFY_USER_ID = "xq7dp9tfkzicm5s"
     SPOTIFY_ACCESS_TOKEN = os.environ.get("SPOTIFY_ACCESS_TOKEN", "")
     SPOTIFY_BASE_URL = 'https://api.spotify.com/v1'
